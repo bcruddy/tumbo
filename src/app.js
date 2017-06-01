70c96f3de94f6f4538092739cbcc2bb124923107
brian ruddy <briancruddy@gmail.com>
2017-06-01 08:33:32 -0400

    move client side js into src directory

---
+++ src/app.js
@@ -0,0 +1,100 @@
+let tumbo = window.tumbo || {};
+
+tumbo = {
+    init: function () {
+        const userName = prompt('Enter username');
+
+        document.getElementById('current-user').textContent = userName;
+
+        const socket = io(),
+            chatForm = document.getElementById('chat-form'),
+            responseList = document.getElementById('server-response'),
+            createResponseItem = (res, index = -1) => {
+                let data = JSON.parse(res),
+                    listItem = document.createElement('li');
+
+                listItem.dataset.index = index;
+                listItem.textContent = data.message;
+
+                listItem.innerHTML = `
+                    <strong class="username">${data.user}:</strong>
+                    <small class="date">(${data.date})</small>
+                    <span class="message">${data.message}</span>`;
+
+                return listItem;
+            };
+
+        chatForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            let message = document.getElementById('m'),
+                data = JSON.stringify({
+                    user: userName,
+                    message: message.value,
+                    date: new Date()
+                });
+
+            socket.emit('chat message', data);
+
+            message.value = '';
+
+            return false;
+        });
+
+        socket.on('chat message', msg => {
+            responseList.appendChild(createResponseItem(msg));
+        });
+
+        var webcamJscii = new Jscii({
+            container: document.getElementById('ascii-container-webrtc'),
+            el: document.getElementById('jscii-element-webrtc'),
+            webrtc: true,
+            interval: 333,
+            fn: function (ascii) {
+                socket.emit('ascii video', ascii);
+            }
+        });
+
+        const chatConnections = {};
+
+        socket.on('ascii video', function (data) {
+            var videoWindow;
+            if (Object.keys(chatConnections).indexOf(data.id) === -1) {
+                chatConnections[data.id] = socket;
+
+                var wrapper = document.createElement('div');
+                wrapper.classList.add('col-md-6');
+
+                videoWindow = document.createElement('pre');
+
+                videoWindow.setAttribute('data-socket-id', data.id);
+                videoWindow.classList.add('video-pane');
+
+                wrapper.appendChild(videoWindow);
+
+                document.getElementById('video-windows').appendChild(wrapper);
+            } else {
+                videoWindow = document.querySelector(`.video-pane[data-socket-id="${data.id}"]`);
+            }
+
+            // check to see if socket id is already being displayed
+            // if not, create a new display
+            videoWindow.innerHTML = data.ascii;
+        });
+
+        socket.on('disconnect', function (socketId) {
+            if (socketId in chatConnections) {
+                delete chatConnections[socketId];
+                var videoWindow = document.querySelector(`.video-pane[data-socket-id="${socketId}"]`);
+                videoWindow.parentNode.parentNode.removeChild(videoWindow.parentNode);
+            }
+        });
+
+        document.getElementById('play-webrtc').addEventListener('click', function() {
+            webcamJscii.play();
+        });
+        document.getElementById('pause-webrtc').addEventListener('click', function() {
+            webcamJscii.pause();
+        });
+    }
+}
